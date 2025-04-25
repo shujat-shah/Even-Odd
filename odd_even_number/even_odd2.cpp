d536d8cacd12d3007c00b96d456c1342c662c664
shujatXshadow <shahalichitral072@gmail.com>
2025-04-25 21:28:40 +0500

    Add files via upload
    This simple C++ program checks whether a given number is even or odd using a user-defined function and the ternary operator.
    
    💡 Features:
    Uses modular arithmetic to check divisibility by 2.
    
    Demonstrates the use of:
    
    Functions
    
    Boolean return types
    
    Conditional (ternary) operator
    
    Input/output with cin and cout

---
+++ odd_even_number/even_odd2.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+
+bool check(int num){
+return num % 2 == 0? true: false;
+}
+int main() {
+    int num;
+    cout << "Enter a number";
+    cin>>num;cout<< endl;
+    check(num)?cout<<"Even Number":cout<<"Odd number";
+    return 0;
+}
